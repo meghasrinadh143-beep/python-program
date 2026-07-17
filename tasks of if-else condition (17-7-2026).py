8bdcd3fa24b626047a5ef25c6c1cf6a1bed11dc0
megha srinadh <meghasrinadh143@gmail.com>
2026-07-17 15:50:06 +0530

    Add files via upload

---
+++ tasks of if-else condition (17-7-2026).py
@@ -0,0 +1,49 @@
+#task
+#1 voting eligible
+'''age=int(input("enter age:"))
+if age>18:
+    print("eligible for voting")
+else:
+    print("not eligible")'''
+
+#2 find the number is odd or even
+'''a=int(input("enter a value:"))
+if a%2==0:
+    print("even")
+elif a%2!=0:
+    print("odd")
+else:
+    print("number in valid")'''
+
+
+#3 find the year is leap year or not
+'''year=int(input("enter year:"))
+if year%4==0:
+    print("leap year")
+else:
+    print("not a leap year")'''
+
+#4  guest code
+'''name=input("enter name:")
+if name=="srinadh":
+    print("welcome srinadh")
+else:
+    print("welcome guest")'''
+
+#5 print a specified name with welcome 
+'''name=["srinadh","lokanadh","kavith","kumar","vyshnai"]
+a=input("enter name:")
+if a in name:
+    print("welcome",a)
+else:
+    print("welcome guest")'''
+
+#6 vowels or consonent
+a=["a","e","i","o","u"]
+b=input("enter alphabet")
+if b in a:
+    print("given alphabet is vowel")
+else:
+    print("given alphabet is consonent")
+
+
