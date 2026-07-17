df40572a314d34a33eab0454f92cb7247de522d4
megha srinadh <meghasrinadh143@gmail.com>
2026-07-17 15:46:15 +0530

    Add files via upload

---
+++ if-elif-else condition.py
@@ -0,0 +1,60 @@
+#if-elif condition by using comparision
+'''a=2
+b=4
+if a<b:
+    print("less")
+elif b>a:
+    print("grater")'''
+\
+
+'''a=2
+b=4
+if a==b:
+    print("equal")
+elif b>a:
+    print("greater")
+elif a!b:
+    print("not equal")'''
+
+
+'''a=2
+b=4
+if a==b:
+    print("equal")
+elif b<a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+
+#if=elif-else
+
+'''a=5
+b=10
+if a>b:
+    print("greater")
+elif a>=b:
+    print("greater and equal")
+elif a==b:
+    print("equal")
+else:
+    print("true")'''
+
+
+'''a=5
+b=10
+if a<b and b>a:
+    print("greater")
+elif a!=b or a==b:
+    print("less or equal")
+else:
+    print("true")'''
+
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+if a<b:
+    print("less")
+elif a>b:
+    print("greater")
+else:
+    print("true")
---
+++ multiple if.py
@@ -0,0 +1,21 @@
+#nested if
+'''a=9
+b=11
+if a>b:
+    print("less")
+if a<b:
+    print("greater")
+if a!=b:
+    print("not equal")
+if a==b:
+    print("equal")'''
+
+a=9
+b=11
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a==b:
+    print("equal")
+    
