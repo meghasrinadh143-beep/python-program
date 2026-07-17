7c19137256252e0490176ee5b00b50bcd9179c55
megha srinadh <meghasrinadh143@gmail.com>
2026-07-17 14:09:53 +0530

    Add files via upload

---
+++ app of data type.py
@@ -0,0 +1,189 @@
+'''a=2
+b=4
+ptint(a+b)'''
+
+
+'''a=3
+b=4
+c=a+b
+print(c)'''
+
+
+#print(10+10)
+#run_time input()
+
+
+'''a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+print(a+b)'''
+
+
+'''a=float(input("enter a value:"))
+b=float(input("enter b value:"))
+print(a+b)'''
+
+
+
+'''a=str(input("enter a value:"))
+b=str(input("enter b value:"))
+print(a+b)'''
+
+
+
+'''a=(input("enter a value:"))
+b=(input("enter b value:"))
+print(a+b)'''
+
+
+
+'''a=complex(input("enter a value:"))
+b=complex(input("enter b value:"))
+print(a+b)'''
+
+
+
+'''a=bool(input("enter a value:"))
+b=bool(input("enter b value:"))
+print(a+b)'''
+
+
+'''fname=input("first name:")
+lname=input("last name:")
+print(fname+" "+lname)'''
+
+
+'''city=input("city name:")
+print(city)'''
+
+
+
+'''a=bool(input("enter a value:"))
+b=bool(input("enter b value:"))
+c=bool(input("enter c value:"))
+print(a+b+c)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the options 1.add 2.sub 3,mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the options
+        1.add
+        2.sub
+        3,mul"))#if we are taking options in different line then we use ('''''')
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input("choose the options add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+#swaping of two variables
+'''a=5
+b=4
+a,b=b,a
+print("a vlaue is",a)
+print("b value is",b)'''
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("a value is",a)
+print("b value is",b)'''
+
+
+'''a=11
+b=21
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)'''
+
+
+'''a=31
+b=21
+a=a+b
+b=a-b
+a=a-b
+print("after swaping a=%d,b=%d"%(a,b))'''
+
+#converting list to str then using upper to print every letter in capital leter
+'''a=["codegnan","python","course"]
+b=str(a)
+print(b.upper())''' #output["CODEGNAN","PYTHON","COURSE"]
+
+'''a=[10,20,30,40,50,"code"]
+a.extend("code")
+print(a)'''#output([10,20,30,40,50,"code","c","o","d","e"])
+
+'''a=[5,7,9,10,11,13]
+a.insert(5,12)
+print(a)'''#output([5,7,9,10,11,12,13])
+
+
+'''a=[9,7,4,0,1,5,10,8,6,3]
+a.sort()
+print(a)'''#output([0,1,3,4,5,6,7,8,9,10])
+
+
+'''a=("apple","banana","mango")
+b=list(a)
+b.append("graps")
+c=tuple(b)
+print(c)
+print(type(c))'''
+
+
+#student profile code
+'''id_no=str(input("enter the id:"))
+name=str(input("enter name:"))
+mobile_no=int(input("enter mobile no:"))
+mail=str(input("enter mail:"))
+college_name=str(input("enter college name:"))
+branch=str(input("enter branch:"))
+print(".........student profile...........")
+print("id no is ",id_no)
+print("name is",name)
+print("mobile no is ",mobile_no)
+print("mail is ",mail)
+print("college name is ",college_name)
+print("branch is",branch)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ if and if-else conditon.py
@@ -0,0 +1,217 @@
+#condition statment
+#if-condition by using comparision statment
+# <,>,<=,>=,!=,==
+'''a=10
+b=15
+if a<b:
+    print("true")'''
+
+'''a=6
+b=9
+if a>b:
+    print("true")'''
+
+'''a=12
+b=14
+if a<=b:
+    print("true")'''
+
+    
+'''a=12
+b=11
+if a>=b:
+    print("true")'''
+    
+'''a=21
+b=23
+if a!=b:
+    print("true")'''
+
+
+'''a=22
+b=22
+if a==b:
+    print("true")'''
+    
+'''a=3
+if a>2:
+    print("true")'''
+
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+if a<b:
+    print("true")'''
+
+'''a=input("enter string :")
+b=input("enter string :")
+if a!=b:
+    print("true")'''#with string we can only use !=,== in if condition
+
+
+#if-condition by using logical operator 
+'''a=5
+b=8
+if a<b and b>a:
+    print("true")'''
+
+
+'''a=9
+b=11
+if a<=b and b>=a:
+    print("true")'''
+
+'''a=4
+b=11
+if a!=b and a==b:
+    print("true")'''
+
+
+'''a=4
+b=11
+if a<b or a>b:
+    print("true")'''
+
+'''a=4
+b=11
+if a<=b or a>=b:
+    print("true")'''
+
+'''a=4
+b=11
+if a!=b or a==b:
+    print("true") '''
+
+'''a=5
+b=9
+if not a>b:
+    print("true")'''
+
+
+'''a=int(input("enter a value :"))
+b=int(input("enter b value :"))
+if a<=b or a>=b:
+    print("true")'''
+
+#if-condition by using identify operators
+
+
+#is #is not
+
+
+'''a=7
+if type(a) is int:
+    print("true")'''
+
+'''a=9
+if type(a) is not int:
+    print("true")'''
+
+'''a=int(input("enter a value :"))
+b=int(input("enter b value :"))
+
+if type(a) is int:
+    print("true")'''
+
+
+'''a=float(input("enter value"))
+b=float(input("enter value"))
+
+if type(b) is float:
+    print("true")'''
+
+'''a=input("enter a :")
+if type(a):
+    print("true")'''
+
+#if-condition by using membership operation
+#in,not in
+
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+    
+'''a=2,3,4,5,6,7,8,9,0
+if 10 in a:
+    print("true4")'''
+
+
+#
+'''a=int(input())
+
+if 30 in a:
+    print("true")'''
+#errror
+
+
+
+'''a=(4,5,6,7,8,9,0,2,3,4)
+b=int(input("enter a value"))
+
+if b in a:
+    print("true")'''
+
+# if-else conditon by using comparision condition
+
+'''a=3
+b=6
+if a<b:
+    print("a<b")
+else:
+        print("true")'''
+'''a=3
+b=6
+if a>b:
+    print("a>b")
+else:
+        print("true")'''
+'''a=3
+b=6
+if a<=b:
+    print("a<=b")
+else:
+    print("true")'''
+
+    
+'''a=3
+b=6
+if a>=b:
+    print("a>=b")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if a!=b:
+    print("a!=b")
+else:
+    print("true")'''
+
+'''a=3
+b=3
+if a==b:
+    print("a==b")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if a<b and b>a:
+    print("a<b,a>b")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if a<=b or a>=b:
+    print("a<=b,a>=b")
+else:
+    print("true")'''
+
+'''a=3
+b=6
+if not a>b:
+    print("a>b")
+else:
+    print("true")'''
+    
+
